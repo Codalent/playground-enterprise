a1ffa7b48a4822da96fa6302772e129d30a67b04
manishayd22 <manishayd22@gmail.com>
2024-07-19 11:06:54 +0200

    Created Header and Footer

--- config/krakend/demo-redesign/src/components/common/Footer/index.tsx
+++ config/krakend/demo-redesign/src/components/common/Footer/index.tsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-  return <div className="bg-brand-neutral-900 text-white">Footer</div>;
-};
+  return (
+    <section className="bg-brand-neutral-900">
+      <div className="container--boxed-xl flex flex-col items-center justify-betwee w-full">
+        <div className="bg-slate-600 border w-full"></div>
+        <div className="relative px-0 py-4 lg:pb-10 lg:pt-8 w-full">
+          <div className=" text-white flex items-center justify-between">
+            <p className="text-slate-400 text--lg" style={{ maxWidth: '722px' }}>
+              Copyright 2017 - 2024 KRAKEND S.L.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Footer;
+export default Footer
--- config/krakend/demo-redesign/src/components/common/Header/index.tsx
+++ config/krakend/demo-redesign/src/components/common/Header/index.tsx
@@ -1,5 +1,41 @@
+import ExternalIcon from '@/image/images/icons/icon-external.svg'
+import KrakendLogo from '@/image/images/logos/logo-krakend-bw.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 const Header = () => {
-  return <div className="bg-brand-neutral-900 text-white">Header</div>;
-};
+  return (
+    <section className="bg-brand-neutral-900">
+      <div className="container--boxed-xl">
+        <div className="relative px-0 py-4 lg:py-6  ">
+          <div className="text-white flex items-center justify-between h-full">
+            <div className="relative z-10">
+              <Image src={KrakendLogo} alt="KrakendLogo" />
+            </div>
+            <div className="absolute inset-0 z-0 flex items-center justify-end">
+              <ul className="flex items-center justify-between">
+                <li className="mx-2 md:mx-4">
+                  <Link href={''} className="font-medium text-base">
+                    <span className="flex items-center">
+                      Docs
+                      <Image src={ExternalIcon} alt="" />
+                    </span>
+                  </Link>
+                </li>
+                <li className="mx-2 md:mx-4">
+                  <Link href={''} className="font-medium text-base">
+                    <span className="flex items-center">
+                      Website
+                      <Image src={ExternalIcon} alt="" />
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Header;
+export default Header
