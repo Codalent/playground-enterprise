99f0d121f87e4f7c745afe8c2d2e790e34da6922
Arpit Bansal <arpit@codalent.com>
2024-11-08 19:24:34 +0530

    feat: move from app router to page router

--- config/krakend/demo-redesign/src/components/Home/UseCases/index.tsx
+++ config/krakend/demo-redesign/src/components/Home/UseCases/index.tsx
@@ -1,74 +1,43 @@
-import userCases from "@/data/krakend.json";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-const UseCases = () => {
-  const [useCases, setUseCases] = useState<any>({});
 
-  // For create the structured data
-  const createDataHandler = () => {
-    const dataObj: any = {};
-    userCases.endpoints.forEach((singleUserCase) => {
-      if (
-        singleUserCase?.extra_config?.["documentation/openapi"]?.tags?.length
-      ) {
-        singleUserCase?.extra_config?.["documentation/openapi"]?.tags.forEach(
-          (tag) => {
-            if (dataObj.hasOwnProperty(tag)) {
-              dataObj[tag] = [...dataObj[tag], singleUserCase];
-            } else {
-              dataObj[tag] = [singleUserCase];
-            }
-          }
-        );
-      } else {
-        dataObj["others"] = dataObj["others"]
-          ? [...dataObj["others"], singleUserCase]
-          : [singleUserCase];
-      }
-    });
-    setUseCases(dataObj);
-  };
-
-  useEffect(() => {
-    createDataHandler();
-  }, []);
-
-  return Object.keys(useCases).length > 0 ? (
+const UseCases = ({ data }) => {
+  return Object.keys(data).length > 0 ? (
     <div className="py-5 px-6 rounded-xl" style={{ background: "#171921" }}>
       <p className="text-white mb-4 font-medium">
         Explore demos for these use cases:
       </p>
 
-      {Object.entries(useCases).map((useCaseData: any) => {
+      {Object.keys(data).map((key: string) => {
+        if (key === "others") return null;
         return (
-          useCaseData[0] !== "others" && (
-            <div
-              className="p-4 rounded-md mb-6 border border-brand-neutral-600"
-              key={useCaseData[0]}
-            >
-              <p className="text-white font-medium mb-4">{useCaseData[0]}</p>
-              <div className="flex flex-col gap-4">
-                {useCaseData[1].map((useCase: any, index: number) => {
-                  return (
-                    <Link
-                      href={`/use-cases/${useCase.custom_fields.slug}`}
-                      className="text-white text-base p-3 rounded-md hover:outline hover:outline-brand-neutral-300 transition-colors shadow-md break-words"
-                      key={`${useCase.custom_fields.slug}-${index}`}
-                      style={{ background: "#272B3A" }}
-                    >
-                      {useCase.title}
-                    </Link>
-                  );
-                })}
-              </div>
+          <div
+            className="p-4 rounded-md mb-6 border border-brand-neutral-600"
+            key={key}
+          >
+            <p className="text-white font-medium mb-4 capitalize">
+              {key.replace("_", " ")}
+            </p>
+            <div className="flex flex-col gap-4">
+              {data[key].map((useCase: any, index: number) => {
+                return (
+                  <Link
+                    href={`/use-cases/${useCase.custom_fields.slug}`}
+                    className="text-white text-base p-3 rounded-md hover:outline hover:outline-brand-neutral-300 transition-colors shadow-md break-words"
+                    key={`${useCase.custom_fields.slug}-${index}`}
+                    style={{ background: "#272B3A" }}
+                  >
+                    {useCase.custom_fields.name}
+                  </Link>
+                );
+              })}
             </div>
-          )
+          </div>
         );
       })}
 
       <div className="flex flex-col gap-4">
         <p className="text-white font-medium">Others</p>
-        {useCases["others"].map((useCase: any, index: number) => {
+        {data["others"].map((useCase: any, index: number) => {
           return (
             <Link
               href={`/use-cases/${useCase.custom_fields.slug}`}
@@ -76,7 +45,7 @@ const UseCases = () => {
               className="text-white text-base p-3 rounded-md hover:outline hover:outline-brand-neutral-300 transition-colors shadow-md break-words"
               style={{ background: "#272B3A" }}
             >
-              {useCase.title}
+              {useCase.custom_fields.name}
             </Link>
           );
         })}
